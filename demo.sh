7edeb7ea66147a8612583fad529844884fca344d
johnburris <john.burris512@gmail.com>
2025-01-13 19:15:57 -0600

    demo.sh demo.png

--- demo.sh
+++ demo.sh
@@ -5,7 +5,7 @@
 
 echo
 
-echo "BASIC LATIN:"
+echo "# BASIC LATIN:"
 echo
 echo "! \" # $ % & ' ( ) * + , - . /"
 echo "0 1 2 3 4 5 6 7 8 9"
@@ -16,7 +16,7 @@ echo "{ | } ~"
 
 echo
 
-echo "LATIN-1 SUPPLEMENT:"
+echo "# LATIN-1 SUPPLEMENT:"
 echo
 echo "¡ ¢ £ ¤ ¥ ¦ § ¨ © ª « ¬ ­ ® ¯ ° ± ² ³ ´ µ ¶ · ¸ ¹ º » ¼ ½ ¾ ¿"
 echo "À Á Â Ã Ä Å Æ Ç È É Ê Ë Ì Í Î Ï Ð Ñ Ò Ó Ô Õ Ö × Ø Ù Ú Û Ü Ý Þ ß"
@@ -24,31 +24,31 @@ echo "à á â ã ä å æ ç è é ê ë ì í î ï ð ñ ò ó ô õ ö ÷ ø
 
 echo
 
-echo "GENERAL PUNCTUATION:"
+echo "# GENERAL PUNCTUATION:"
 echo
 echo "– —"
 
 echo
 
-echo "MATHEMATICAL OPERATORS:"
+echo "# MATHEMATICAL OPERATORS:"
 echo
 echo "≠ ≤ ≥ ≦ ≧"
 
 echo
 
-echo "MISCELLANEOUS SYMBOLS:"
+echo "# MISCELLANEOUS SYMBOLS:"
 echo
 echo "☦"
 
 echo
 
-echo "DINGBATS:"
+echo "# DINGBATS:"
 echo
 echo "✝ ✞"
 
 echo
 
-echo "HALFWIDTH AND FULLWIDTH FORMS"
+echo "# HALFWIDTH AND FULLWIDTH FORMS:"
 echo
 echo "｡ ｢ ｣ ､ ･ ｦ ｧ ｨ ｩ ｪ ｫ ｬ ｭ ｮ ｯ ｰ"
 echo "ｱ ｲ ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿ ﾀ ﾁ ﾂ ﾃ ﾄ ﾅ ﾆ ﾇ ﾈ ﾉ ﾊ ﾋ ﾌ ﾍ ﾎ ﾏ ﾐ ﾑ ﾒ ﾓ ﾔ ﾕ ﾖ ﾗ ﾘ ﾙ ﾚ ﾛ ﾜ ﾝ"
